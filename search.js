9885f3428c370b06169e83050c5a139c56c262cd
Jayasmita Sahu <96156277+jayasmita12@users.noreply.github.com>
2022-10-15 12:36:22 +0530

    search functionaliy create also puppy page done and also search image display

--- search.js
+++ search.js
@@ -7,15 +7,19 @@ async function particularbreedImage(){
         let res = await fetch(`https://dog.ceo/api/breed/${searchname.value}/images`)
         let data=await res.json()
         console.log(data.message)
-        appendimage(data.message)
+        if(data.message.length >=1){
+            appendimage(data.message)
+        }
+      
     }
     catch{
+        noseachresult()
         console.log("Something went wrong...")
     }
 }
 
+var showimagediv = document.getElementById("search-images")
 function appendimage(images){
-    var showimagediv = document.getElementById("search-images")
     showimagediv.style="display:grid;grid-template-columns: auto auto auto;justify-content:space-evenly;margin-top:30px"
      showimagediv.innerHTML=null
     images.map((e)=>{
@@ -25,4 +29,14 @@ function appendimage(images){
         showimagediv.append(img)
     })
 
+}
+
+function noseachresult(){
+    let div=document.createElement("div")
+    let h1=document.createElement("h1")
+    div.style="margin-left:200px;width:500px;text-align:center"
+    h1.innerHTML="Sorry, No such result found"
+    h1.style="color:red"
+    div.append(h1)
+    showimagediv.append(div)
 }
